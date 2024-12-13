69ed02ef04ed1056c86aa534bff55234868217d2
Glob~Lynn(Kayshoop) <shoopkayla4@gmail.com>
2024-12-12 18:15:30 -0600

    attempting to gather images of stars from Astropix API

--- JAVASCRIPT/page4API.js
+++ JAVASCRIPT/page4API.js
@@ -1,3 +1,27 @@
-// linking NASA Vesta/Moon/Mars Trek WMTS API
+const apiUrl = 'https://www.astropix.org/link/mgv?format=json';
 
+function fetchImageMetadata() {
+    return fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data.map(item => ({
+                url: item.resource_url,
+                title: item.title
+            }));
+        })
+        .catch(error => console.error('Error fetching image metadata:', error));
+}
 
+function displayImages(images) {
+    const imgDisplay = document.getElementById('imgDisplay');
+    imgDisplay.innerHTML = images.map(image => `<img src="${image.url}" alt="${image.title}">`).join('');
+}
+
+window.onload = () => {
+    fetchImageMetadata().then(images => displayImages(images));
+};
