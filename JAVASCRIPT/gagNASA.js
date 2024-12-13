8772a75e18c88860b29273a21c38fc9f022f46b6
Glob~Lynn(Kayshoop) <shoopkayla4@gmail.com>
2024-12-12 22:56:54 -0600

    adding NASA api key

--- JAVASCRIPT/gagNASA.js
+++ JAVASCRIPT/gagNASA.js
@@ -1,2 +1,3 @@
 // put the next line into Postman to view the data the API will be pulling. 
 // GET https://api.nasa.gov/planetary/apod?api_key=H9y4EjyMCZ6uEVoAlsB2hxLXAu3QU64V60qLSwwG&count=15
+// API Key for NASA: H9y4EjyMCZ6uEVoAlsB2hxLXAu3QU64V60qLSwwG
