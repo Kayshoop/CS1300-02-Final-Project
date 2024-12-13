44dd21a9d2e31f549d833609a3396c6606dc8ec0
Glob~Lynn(Kayshoop) <shoopkayla4@gmail.com>
2024-12-12 21:30:03 -0600

    adding cache control header for better preformance. API is linked but not fully pulling data

--- JAVASCRIPT/page4API.js
+++ JAVASCRIPT/page4API.js
@@ -1,7 +1,6 @@
 // geocode API Key 97f28ca84025d05f90a5e70bd9940478
 // Base URL: http://api.positionstack.com/v1/
 
-
 const stormGlassApiKey = 'eb83a106-b8f3-11ef-9159-0242ac130003-eb83a16a-b8f3-11ef-9159-0242ac130003'; 
 const geocodingApiKey = '97f28ca84025d05f90a5e70bd9940478';
 
@@ -20,12 +19,33 @@ function getCoordinates(city) {
     const geocodingUrl = `http://api.positionstack.com/v1/forward?access_key=${geocodingApiKey}&query=${city}`; 
     return fetch(geocodingUrl) 
         .then(geoResponse => geoResponse.json()) 
-        .then(geoData => { if (data.data && data.data.length > 0) { 
-            const { latitude: lat, longitude: lng } = data.data[0]; 
-            return { lat, lng }; 
-        } 
-        else { 
-            throw new Error('Location not found'); 
-        } 
-    }); 
+        .then(geoData => { 
+            if (geoData.data && geoData.data.length > 0) { 
+                const { latitude: lat, longitude: lng } = geoData.data[0]; 
+                return { lat, lng }; 
+            } else { 
+                throw new Error('Location not found'); 
+            } 
+        }); 
+}
+
+function fetchMoonPhase(lat, lng) {
+    const end = new Date().toISOString().split('T')[0];
+    const apiUrl = `https://api.stormglass.io/v2/astronomy/point?lat=${lat}&lng=${lng}&end=${end}`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': stormGlassApiKey,
+            'Cache-Control': 'no-cache'
+        }
+    };
+
+    return fetch(apiUrl, options)
+        .then(response => response.json())
+        .then(data => {
+            const moonPhase = data.data[0].moonPhase.current.text;
+            document.getElementById('moonPhaseEmoji').textContent = moonPhase;
+        })
+        .catch(error => console.error('Error fetching data:', error));
 }
